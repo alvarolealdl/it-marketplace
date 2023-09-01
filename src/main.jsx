20614f4877728d7a5f490846b7cd36e2dac604cd
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-01 15:32:50 -0300

    [ITMKTPL] Update calls in app, main and global imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import MarketplaceRouter from "./MarketplaceRouter.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/styles/_Global.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <MarketplaceRouter />
+  </BrowserRouter>
+);

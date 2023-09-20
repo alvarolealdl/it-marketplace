0755f35dd8c535dad41cca184291c4d72d1d7c53
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-20 10:49:27 -0300

    [ITMKTPL] Refactor

--- src/components/ChoosenLogin.jsx
+++ src/components/ChoosenLogin.jsx
@@ -2,35 +2,39 @@ import { useState } from "react";
 import Button from "../components/Button.jsx";
 import Login from "../views/login/Login.jsx";
 
-const ChoosenLogin = () => {
+const ChoosenLogin = (isAuthenticated) => {
   const [showSimpleLogin, setShowSimpleLogin] = useState(false);
   const [showSsoLogin, setShowSssoLogin] = useState(false);
 
+  console.log("ISAUTH:", isAuthenticated);
+
   return (
     <>
-      <div className="mktp-choose-login container">
-        <p className="my-2">
-          <b>Escolha abaixo o tipo de Login que você deseja fazer:</b>
-        </p>
-        <div className="mktp-choose-login__buttons">
-          <Button
-            type="primary"
-            action="submit"
-            text="Login Simples"
-            onClick={() => {
-              setShowSimpleLogin(true);
-              setShowSssoLogin(false);
-            }}
-          />
-          <Button
-            type="secondary"
-            action="submit"
-            text="Login SSO"
-            onClick={() => {
-              setShowSimpleLogin(false);
-              setShowSssoLogin(true);
-            }}
-          />
+      <div className="mktp-choose-login">
+        <div className="container">
+          <p className="my-2">
+            <b>Escolha abaixo o tipo de Login que você deseja fazer:</b>
+          </p>
+          <div className="mktp-choose-login__buttons">
+            <Button
+              type="primary"
+              action="submit"
+              text="Login Simples"
+              onClick={() => {
+                setShowSimpleLogin(true);
+                setShowSssoLogin(false);
+              }}
+            />
+            <Button
+              type="secondary"
+              action="submit"
+              text="Login SSO"
+              onClick={() => {
+                setShowSimpleLogin(false);
+                setShowSssoLogin(true);
+              }}
+            />
+          </div>
         </div>
       </div>
       {showSimpleLogin && <Login TypeLogin="simple" />}

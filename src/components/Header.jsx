47e393cbda15cfa5ebb7634ee929c3c37f00500d
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-20 10:50:28 -0300

    [ITMKTPL] Update header to be more responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import Form from "react-bootstrap/Form";
 import { toast } from "react-toastify";
 import Button from "./Button";
 import Icon from "./Icon";
@@ -7,7 +9,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Profile from "./Profile";
 import axios from "axios";
 
-const Header = () => {
+const Header = ({ toggleTheme, theme }) => {
   const [showDropdown, setShowDropdown] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const location = useLocation();
@@ -75,93 +77,123 @@ const Header = () => {
   };
 
   return (
-    <div className="mktp-header">
-      <div className="mktp-header__left">
-        <div className="mktp-header__left__logo">
-          <a href="/" onClick={(e) => handleLinkClick(e, "/")}>
-            MERCADO TI
-          </a>
-        </div>
-        {location.pathname !== "/login" &&
-          location.pathname !== "/create-user" && (
-            <div className="mktp-header__left__search">
-              <input
-                type="text"
-                placeholder="Buscar ofertas"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                onKeyUp={handleSearchKeyPress}
-              />
-              <Button type="primary" text="Buscar" onClick={handleSearch} />
+    <>
+      <div className={`mktp-header${theme}`}>
+        <div className={`mktp-header`}>
+          <div className="mktp-header__left">
+            <div className="mktp-header__left__logo">
+              <a href="/" onClick={(e) => handleLinkClick(e, "/")}>
+                MERCADO TI
+              </a>
             </div>
-          )}
-      </div>
-      <div className="mktp-header__right">
-        {isAuthenticated || isLoggedIn ? (
-          <>
-            <div className="mktp-header__right__info">
-              <Profile />
-            </div>
-            <div className="mktp-header__right__logout">
-              {isAuthenticated && (
-                <Button type="logout" text="Logout" onClick={() => logout()} />
-              )}
 
-              {isLoggedIn && (
-                <Button
-                  type="default"
-                  text="Logout"
-                  onClick={handleDirectLogout}
-                />
-              )}
-            </div>
-            <div className="mktp-header__right__menu">
-              <Icon
-                className="pointer"
-                fill={"#fff"}
-                size={40}
-                typeIcon={"list-dashes"}
-                onClick={toggleDropdown}
+            <div className={`mktp-header__left__theme`}>
+              <Form.Check
+                className="mktp-header__left__theme"
+                type="switch"
+                id="custom-switch"
+                label={theme === "-light" ? "Light Theme" : "Dark Theme"}
+                onClick={toggleTheme}
               />
-              {showDropdown && (
-                <div className="mktp-header__right__menu__dropdown">
-                  <ul>
-                    <li>
-                      <a
-                        href="/create-negotiation"
-                        onClick={(e) =>
-                          handleLinkClick(e, "/create-negotiation")
-                        }
-                      >
-                        Criar Negociação
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="/my-negotiations"
-                        onClick={(e) => handleLinkClick(e, "/my-negotiations")}
-                      >
-                        Minhas Negociações
-                      </a>
-                    </li>
-                  </ul>
+            </div>
+          </div>
+          <div className="mktp-header__middle">
+            {location.pathname !== "/login" &&
+              location.pathname !== "/create-user" && (
+                <div className="mktp-header__left__search">
+                  <input
+                    type="text"
+                    placeholder="Buscar ofertas"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyUp={handleSearchKeyPress}
+                  />
+                  <Button type="primary" text="Buscar" onClick={handleSearch} />
                 </div>
               )}
-            </div>
-          </>
-        ) : (
-          <div className="mktp-header__right__auth">
-            <span>
-              Faça&nbsp;
-              <a href="/login">LOGIN</a> ou <br />
-              crie seu&nbsp;
-              <a href="/create-user">CADASTRO</a>
-            </span>
           </div>
-        )}
+          <div className="mktp-header__right">
+            {isAuthenticated || isLoggedIn ? (
+              <>
+                <div className="mktp-header__right__info">
+                  <Profile />
+                </div>
+                <div className="mktp-header__right__logout">
+                  {isAuthenticated && (
+                    <Button
+                      type="logout"
+                      text="Logout"
+                      onClick={() => logout()}
+                    />
+                  )}
+
+                  {isLoggedIn && (
+                    <Button
+                      type="default"
+                      text="Logout"
+                      onClick={handleDirectLogout}
+                    />
+                  )}
+                </div>
+                <div
+                  className="mktp-header__right__menu"
+                  onMouseEnter={toggleDropdown}
+                  onMouseLeave={toggleDropdown}
+                >
+                  <Icon
+                    className="pointer"
+                    fill={theme === "-light" ? "#000" : "#fff"}
+                    size={40}
+                    typeIcon={"list-dashes"}
+                  />
+                  {showDropdown && (
+                    <div className="mktp-header__right__menu__dropdown">
+                      <ul>
+                        <li>
+                          <a
+                            href="/create-negotiation"
+                            onClick={(e) =>
+                              handleLinkClick(e, "/create-negotiation")
+                            }
+                          >
+                            Criar Negociação
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="/my-negotiations"
+                            onClick={(e) =>
+                              handleLinkClick(e, "/my-negotiations")
+                            }
+                          >
+                            Minhas Negociações
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </>
+            ) : (
+              <div className="mktp-header__right__auth">
+                <span>
+                  Faça&nbsp;
+                  <a href="/login">LOGIN</a> ou <br />
+                  crie seu&nbsp;
+                  <a href="/create-user">CADASTRO</a>
+                </span>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
+};
+
+Header.propTypes = {
+  toggleTheme: PropTypes.func,
+  theme: PropTypes.string,
 };
 
 export default Header;

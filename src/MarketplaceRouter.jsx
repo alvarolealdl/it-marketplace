0f29166f64510c6dd588da12d74f4bd2116ea844
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-23 07:31:21 -0300

    [ITMKTPL] Add footer component

--- src/MarketplaceRouter.jsx
+++ src/MarketplaceRouter.jsx
@@ -10,6 +10,7 @@ import CreateNegotiation from "./views/create-negotiation/CreateNegotiation.jsx"
 import MyNegotiations from "./views/my-negotiations/MyNegotiations.jsx";
 import ViewNegotiation from "./views/view-negotiation/ViewNegotiation.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
+import Footer from "./components/Footer.jsx";
 
 const MarketplaceRouter = () => {
   const { user, isAuthenticated } = useAuth0();
@@ -37,33 +38,36 @@ const MarketplaceRouter = () => {
 
   return (
     <div className={`mktp-container${theme}`}>
-      <Header toggleTheme={toggleTheme} theme={theme} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<ChoosenLogin />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create-user" element={<CreateUser />} />
-        <Route
-          path="/create-negotiation"
-          element={
-            isAuthenticated ? (
-              <CreateNegotiation userId={user} userEmail={user.email} />
-            ) : (
-              <CreateNegotiation />
-            )
-          }
-        />
-        <Route
-          path="/negotiation/:negotiationId"
-          element={<ViewNegotiation theme={theme} />}
-        />
+      <div className="mktp-content">
+        <Header toggleTheme={toggleTheme} theme={theme} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<ChoosenLogin />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-user" element={<CreateUser />} />
+          <Route
+            path="/create-negotiation"
+            element={
+              isAuthenticated ? (
+                <CreateNegotiation userId={user} userEmail={user.email} />
+              ) : (
+                <CreateNegotiation />
+              )
+            }
+          />
+          <Route
+            path="/negotiation/:negotiationId"
+            element={<ViewNegotiation theme={theme} />}
+          />
 
-        <Route path="/my-negotiations/" element={<MyNegotiations />} />
-        <Route
-          path="/my-negotiations/:id"
-          element={<ViewNegotiation theme={theme} />}
-        />
-      </Routes>
+          <Route path="/my-negotiations/" element={<MyNegotiations />} />
+          <Route
+            path="/my-negotiations/:id"
+            element={<ViewNegotiation theme={theme} />}
+          />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="d-flex align-items-center justify-content-between">
+      <div className="mktp-footer d-flex flex-column p-3">
+        <p className="mktp-footer__powered mb-2">Powered by Vibbra</p>
+        <p className="mktp-footer__copyright">
+          © 2023 Álvaro Leal de Lima. Todos os Direitos Reservados
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
